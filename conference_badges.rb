0975747cfa515f3fcd969ffd62bc4c5bd857eb60
Garth Lilly <garff247@gmail.com>
2019-04-14 19:52:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+new_array = array
+  for name in new_array
+    new_name = "Hello, my name is #{name}."
+    new_name
+  end
+end
+
+def assign_rooms(array)
+  array.each_with_index {|val, index| puts "Hello #{val}! You'll be assigned to room #{index}!" if index < 10}
+
+end
+
+def printer
+
+end
